78e9382dc89899bad4df0eaacb051908ac5dec6a
samimhamza <samim.hamza7@gmail.com>
2022-04-22 11:09:10 +0430

    http_only_auth

---
+++ src/pages/_app.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import { Provider } from "react-redux";
+import { useStore } from "../store";
+
+const App = ({ Component, pageProps }) => {
+	const store = useStore(pageProps.initialReduxState);
+	return (
+		<Provider store={store}>
+			<Head>
+				<title>Authentication App</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link
+					href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+					rel="stylesheet"
+					integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+					crossorigin="anonymous"
+				></link>
+				<script
+					src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+					integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+					crossorigin="anonymous"
+				></script>
+			</Head>
+			<Component {...pageProps} />;
+		</Provider>
+	);
+};
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+const homePage = () => (
+	<div>
+		<h1>Home Page</h1>
+	</div>
+);
+export default homePage;
